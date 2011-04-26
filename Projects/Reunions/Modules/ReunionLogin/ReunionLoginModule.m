118b245374aa6a9ef69055c08d16f0d623061402
sonya huang <sonya.huang@modolabs.com>
2011-04-26 12:08:54 -0400

    UI tweaks to iphone home screen.

--- Projects/Reunions/Modules/ReunionLogin/ReunionLoginModule.m
+++ Projects/Reunions/Modules/ReunionLogin/ReunionLoginModule.m
@@ -6,6 +6,7 @@
 #import "ReunionHomeModule.h"
 #import "KGOAppDelegate+ModuleAdditions.h"
 #import "Foundation+KGOAdditions.h"
+#import <QuartzCore/QuartzCore.h>
 
 #define IPAD_CURRENTUSER_FONT 20
 
@@ -42,42 +43,24 @@
     UILabel *titleLabel = nil;
     UILabel *subtitleLabel = nil;
     
-    CGFloat y = 10; // iphone only
-    
-    if (subtitle) {
-        UIFont *font;
-        CGRect frame;
-        if (navStyle == KGONavigationStylePortlet) {
-            font = [[KGOTheme sharedTheme] fontForThemedProperty:KGOThemePropertyBodyText];
-            CGSize size = [subtitle sizeWithFont:font constrainedToSize:widget.frame.size];
-            frame = CGRectMake(10, y, size.width, size.height);
-        } else {
-            subtitle = [NSString stringWithFormat:@"%@ : ", subtitle];
-            font = [UIFont boldSystemFontOfSize:IPAD_CURRENTUSER_FONT];
-            CGSize size = [subtitle sizeWithFont:font];
-            frame = CGRectMake(0, 0, size.width, size.height);
-        }
-        
-        subtitleLabel = [[[UILabel alloc] initWithFrame:frame] autorelease];
-        subtitleLabel.font = font;
-        subtitleLabel.backgroundColor = [UIColor clearColor];
-        subtitleLabel.textColor = [UIColor whiteColor];
-        subtitleLabel.text = subtitle;
-        
-        y += subtitleLabel.frame.size.height + 10;
-        
-        [widget addSubview:subtitleLabel];
-    }
+    CGFloat y = 18; // iphone only
     
     if (title) {
         if (navStyle == KGONavigationStylePortlet) {
-            UIFont *font = [[KGOTheme sharedTheme] fontForThemedProperty:KGOThemePropertyContentTitle];
+            //UIFont *font = [[KGOTheme sharedTheme] fontForThemedProperty:KGOThemePropertyContentTitle];
+            UIFont *font = [UIFont fontWithName:@"Georgia" size:24];
             titleLabel = [UILabel multilineLabelWithText:title font:font width:widget.frame.size.width];
+            titleLabel.layer.shadowColor = [[UIColor blackColor] CGColor];
+            titleLabel.layer.shadowOffset = CGSizeMake(0, 1);
+            titleLabel.layer.shadowOpacity = 0.75;
+            titleLabel.layer.shadowRadius = 1;
             CGRect frame = titleLabel.frame;
-            frame.origin.x = 10;
+            frame.origin.x = 3;
             frame.origin.y = y;
             titleLabel.frame = frame;
             titleLabel.textColor = [UIColor whiteColor];
+            
+            y += titleLabel.frame.size.height + 6;
             
         } else {
             UIFont *font = [UIFont boldSystemFontOfSize:IPAD_CURRENTUSER_FONT];
@@ -99,6 +82,35 @@
         }
         
         [widget addSubview:titleLabel];
+    }
+    
+    if (subtitle) {
+        UIFont *font;
+        CGRect frame;
+        if (navStyle == KGONavigationStylePortlet) {
+            font = [UIFont systemFontOfSize:17];
+            CGSize size = [subtitle sizeWithFont:font constrainedToSize:widget.frame.size];
+            frame = CGRectMake(3, y, size.width, size.height);
+        } else {
+            subtitle = [NSString stringWithFormat:@"%@ : ", subtitle];
+            font = [UIFont boldSystemFontOfSize:IPAD_CURRENTUSER_FONT];
+            CGSize size = [subtitle sizeWithFont:font];
+            frame = CGRectMake(0, 0, size.width, size.height);
+        }
+        
+        subtitleLabel = [[[UILabel alloc] initWithFrame:frame] autorelease];
+        subtitleLabel.font = font;
+        subtitleLabel.backgroundColor = [UIColor clearColor];
+        subtitleLabel.textColor = [UIColor whiteColor];
+        subtitleLabel.text = subtitle;
+        subtitleLabel.layer.shadowColor = [[UIColor blackColor] CGColor];
+        subtitleLabel.layer.shadowOffset = CGSizeMake(0, 1);
+        subtitleLabel.layer.shadowOpacity = 0.75;
+        subtitleLabel.layer.shadowRadius = 1;
+        
+        y += subtitleLabel.frame.size.height + 10;
+        
+        [widget addSubview:subtitleLabel];
     }
     
     if (navStyle == KGONavigationStyleTabletSidebar) {
@@ -142,19 +154,20 @@
     if (text) {
         size = [text sizeWithFont:font];
     }
-    
-    UILabel *yearLabel = [[[UILabel alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)] autorelease];
+
+    CGFloat y;
+    UILabel *yearLabel = [[[UILabel alloc] initWithFrame:CGRectMake(0, 4, size.width, size.height)] autorelease];
     yearLabel.text = text;
     yearLabel.font = font;
     yearLabel.textColor = [UIColor whiteColor];
     yearLabel.backgroundColor = [UIColor clearColor];
-    CGFloat y = size.height;
+    y = size.height + 4;
     
     // reunion number superscript
     text = @"th";
     font = [UIFont fontWithName:@"Georgia" size:18];
     size = [text sizeWithFont:font];
-    UILabel *yearSupLabel = [[[UILabel alloc] initWithFrame:CGRectMake(0, 5, size.width, size.height)] autorelease];
+    UILabel *yearSupLabel = [[[UILabel alloc] initWithFrame:CGRectMake(0, 9, size.width, size.height)] autorelease];
     yearSupLabel.text = text;
     yearSupLabel.font = font;
     yearSupLabel.textColor = [UIColor whiteColor];
@@ -191,7 +204,7 @@
     CGRect frame = imageView.frame;
     CGRect springboardFrame = [(KGOHomeScreenViewController *)[appDelegate homescreen] springboardFrame];
     if (navStyle == KGONavigationStylePortlet) {
-        frame.origin.x = springboardFrame.size.width - frame.size.width - 10;
+        frame.origin.x = springboardFrame.size.width - frame.size.width - 2;
     } else {
         frame.origin.x = 36;
     }
