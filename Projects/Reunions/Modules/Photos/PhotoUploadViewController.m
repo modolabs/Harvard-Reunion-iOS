bbab6fdce620d64dbb8b4f0cc08d4d17329fe6fd
sonya huang <sonya.huang@modolabs.com>
2011-05-01 17:51:21 -0400

    facebook photo upload ui.

--- Projects/Reunions/Modules/Photos/PhotoUploadViewController.m
+++ Projects/Reunions/Modules/Photos/PhotoUploadViewController.m
@@ -3,19 +3,82 @@
 #import "FacebookPhotosViewController.h"
 #import <QuartzCore/QuartzCore.h>
 #import "KGOAppDelegate.h"
+#import "UIKit+KGOAdditions.h"
+
+@implementation FacebookPhotoCaptionViewController
+
+@synthesize parentVC;
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    _textView.text = self.parentVC.caption;
+    _textView.textColor = [UIColor darkTextColor];
+}
+
+- (void)textViewDidBeginEditing:(UITextView *)textView
+{
+    // don't adopt parent's behavior which erases the text
+}
+
+- (IBAction)submitButtonPressed:(id)sender
+{
+    self.parentVC.caption = _textView.text;
+    [self.parentVC dismissModalViewControllerAnimated:YES];
+}
+
+@end
+
+
 
 @implementation PhotoUploadViewController
 
-@synthesize photo, profile, parentVC;
+@synthesize photo, profile, parentVC, captionButton = _captionButton;
 
-- (IBAction)submitButtonPressed:(UIButton *)sender {
-    _loadingView.hidden = NO;
+- (NSString *)caption
+{
+    return _caption;
+}
+
+- (void)setCaption:(NSString *)caption
+{
+    [_caption release];
+    _caption = [caption retain];
     
+    if (_caption.length) {
+        [_captionButton setTitle:_caption forState:UIControlStateNormal];
+        [_captionButton setTitle:_caption forState:UIControlStateHighlighted];
+        [_captionButton setTitleColor:[UIColor darkTextColor] forState:UIControlStateNormal];
+        [_captionButton setTitleColor:[UIColor darkTextColor] forState:UIControlStateHighlighted];
+    } else {
+        [_captionButton setTitle:@"Add a caption..." forState:UIControlStateNormal];
+        [_captionButton setTitle:@"Add a caption..." forState:UIControlStateHighlighted];
+        [_captionButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        [_captionButton setTitleColor:[UIColor grayColor] forState:UIControlStateHighlighted];
+    }
+}
+
+- (void)uploadButtonPressed:(id)sender
+{
+    _loadingView.hidden = NO;
+    [_spinner startAnimating];
+
     [[KGOSocialMediaController facebookService] uploadPhoto:self.photo
                                           toFacebookProfile:self.profile
-                                                    message:_textView.text
+                                                    message:self.caption
                                                    delegate:self.parentVC];
-    [_spinner startAnimating];
+}
+
+- (IBAction)captionButtonPressed:(UIButton *)sender {
+    FacebookPhotoCaptionViewController *captionVC = [[[FacebookPhotoCaptionViewController alloc] initWithNibName:@"FacebookCommentViewController" bundle:nil] autorelease];
+    captionVC.parentVC = self;
+    UINavigationController *navC = [[[UINavigationController alloc] initWithRootViewController:captionVC] autorelease];
+    navC.navigationBar.barStyle = UIBarStyleBlack;
+    navC.modalPresentationStyle = UIModalPresentationFormSheet;
+    [self presentModalViewController:navC animated:YES];
+    
+    captionVC.title = self.title;
 }
 
 - (void)cancelButtonPressed:(id)sender {
@@ -35,6 +98,9 @@
 
 - (void)dealloc
 {
+    [_caption release];
+    self.photo = nil;
+    self.profile = nil;
     [super dealloc];
 }
 
@@ -52,26 +118,28 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-
-    // UIImagePickerController comes with a translucent nav bar, which causes
-    // the nav bar to overlap the view
-    if (self.navigationController && self.navigationController.navigationBar.barStyle == UIBarStyleBlackTranslucent) {
-        CGFloat offsetHeight = self.navigationController.navigationBar.frame.size.height;
-        CGRect frame;
-        for (UIView *aView in self.view.subviews) {
-            frame = aView.frame;
-            frame.origin.y += offsetHeight;
-            aView.frame = frame;
-        }
+    
+    self.title = @"Upload Photo";
+    
+    self.navigationItem.rightBarButtonItem = [[[UIBarButtonItem alloc] initWithTitle:@"Upload"
+                                                                               style:UIBarButtonItemStyleDone
+                                                                              target:self
+                                                                              action:@selector(uploadButtonPressed:)] autorelease];
+    if (!self.navigationItem.leftBarButtonItem) {
+        self.navigationItem.leftBarButtonItem = [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel
+                                                                                               target:self
+                                                                                               action:@selector(dismissModalViewControllerAnimated:)] autorelease];
     }
     
-    _textView.layer.cornerRadius = 5.0;
-    _textView.layer.borderColor = [[UIColor blackColor] CGColor];
-    _textView.layer.borderWidth = 1.0;
+    _captionButton.layer.cornerRadius = 5.0;
+    _captionButton.layer.borderColor = [[UIColor blackColor] CGColor];
+    _captionButton.layer.borderWidth = 1.0;
+    
+    [_captionButton setBackgroundImage:[UIImage imageWithPathName:@"common/textfield_button_background"] forState:UIControlStateNormal];
+    
+    self.caption = nil;
     
     _imageView.image = self.photo;
-    
-    [_submitButton setTitle:NSLocalizedString(@"Upload", nil) forState:UIControlStateNormal];
 }
 
 - (void)viewDidUnload
